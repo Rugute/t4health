3c6ee8c8ec2ac0194330a6b87198b649561f5c76
Rugute <rrugute@gmail.com>
2026-01-15 20:13:27 +0300

    inital code

---
+++ src/main/java/com/t4health/signs/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.t4health.signs;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(T4healthApplication.class);
+	}
+
+}
---
+++ src/test/java/com/t4health/signs/T4healthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t4health.signs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T4healthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
